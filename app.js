bd8b07f0d1e60c3b250caedd892db99c982bdb51
Collin Miller Smith <44204853+collin-tx@users.noreply.github.com>
2019-02-11 21:33:01 -0600

    Update app.js

--- app.js
+++ app.js
@@ -3,11 +3,7 @@ const userZip = document.getElementById('zip').toString;
 const button = document.getElementsByClassName("button");
 
 function zipcodeCheck(){
- if (zips.includes(userZip)){
-   return window.alert("We've got your area covered!");
- } else {
-  return window.alert("Sorry, we haven't expanded to your area yet :(");
- }   
+ zips.includes(userZip) ? window.alert("We've got your area covered!") : window.alert("Sorry, we haven't expanded to your area yet :(");
 }
 
 button[0].addEventListener('click', zipcodeCheck);
